69f29ddf53e2492ac24a4370a307e17111a5aaf4
Shriyastar <96578928+Shriyastar@users.noreply.github.com>
2022-01-25 17:15:54 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,13 @@
+class Atm(Object):
+
+    def __init__(self,users_atm_card_number,pin_number):
+        self.users_atm_card_number = users_atm_card_number
+        self.pin_number = pin_number
+
+    def cashWithdrawl(self):
+        print(“Your account No: ……………………………….xxxx is debited with Rs. ………… on ………………towards ATM cash withdrawal. Available balance is Rs. ……………..”)
+        
+    def balanceEnquiry(self):
+        print("For balance enquiry please contact this toll free no:09223766666" Thank You)
+
+        
